5a3494ca8878023e9ee1a4d440e44abd50ed2ad4
Hichamelhmaydi <ct1594065@gmail.com>
2026-01-19 09:45:30 +0100

    add service interface implemtation

---
+++ src/main/java/com/example/ROKO/service/impl/VoyageServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.ROKO.service.impl;
+
+import com.example.ROKO.service.VoyageService;
+import com.example.ROKO.service.VoyageService;
+import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class VoyageServiceImpl implements VoyageService {
+
+}
---
+++ src/main/java/com/example/ROKO/service/impl/VoyageurServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.ROKO.service.impl;
+
+public class VoyageurServiceImpl  {
+}
