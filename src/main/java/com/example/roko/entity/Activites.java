12195de3261e3325edcdf98995696c65a2673445
Hichamelhmaydi <ct1594065@gmail.com>
2026-02-02 09:10:32 +0100

    add class Activites

--- src/main/java/com/example/roko/entity/Activites.java
+++ src/main/java/com/example/roko/entity/Activites.java
@@ -1,4 +1,35 @@
 package com.example.roko.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "activites")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Activites {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nom;
+
+    @Column(length = 1000)
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "voyage_id", nullable = false)
+    private Voyages voyage;
+
+    @ManyToMany(mappedBy = "activites")
+    private Set<Reservations> reservations = new HashSet<>();
+
 }
