0cdb6ffc161f7f263bcf8cf15e4cceec771c8b4e
Hichamelhmaydi <ct1594065@gmail.com>
2026-02-02 09:13:39 +0100

    add class notifications

--- src/main/java/com/example/roko/entity/Notifications.java
+++ src/main/java/com/example/roko/entity/Notifications.java
@@ -1,4 +1,41 @@
 package com.example.roko.entity;
 
+import com.example.roko.enums.NotificationType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notifications")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Notifications {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titre;
+
+    @Column(length = 1000, nullable = false)
+    private String message;
+
+    @Column(nullable = false)
+    private Boolean lu = false;
+
+    @Column(name = "date_creation", nullable = false)
+    private LocalDateTime dateCreation = LocalDateTime.now();
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private NotificationType type;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
