8b236e0964ef1c4c3fb5c31152ac85c97d147509
Hichamelhmaydi <ct1594065@gmail.com>
2026-02-02 11:24:03 +0100

    add repository of activité

--- src/main/java/com/example/roko/repository/ActivitesRepository.java
+++ src/main/java/com/example/roko/repository/ActivitesRepository.java
@@ -1,4 +1,18 @@
 package com.example.roko.repository;
 
-public class ActivitesRepository {
+import com.example.roko.entity.Activites;
+import com.example.roko.entity.Voyages;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.*;
+
+public interface ActivitesRepository extends JpaRepository<Activites, Long> {
+    List<Activites> findAllByActivites_Id(Long id);
+    List<Activites> findByNom(String nom);
+    List<Activites> findByNomContainingIgnoreCase(String nom);
+    Boolean existsByNomAndVoyage_Id(String nom, long  voyage_id);
+    @Query("SELECT COUNT(a) FROM Activites a WHERE a.voyage.id = :voyageId")
+    long countByVoyageId(@Param("voyageId") Long voyageId);
 }
