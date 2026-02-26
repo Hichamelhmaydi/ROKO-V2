90bc8b7494e56356c950a94db58b2d0d59be0ee8
Hichamelhmaydi <ct1594065@gmail.com>
2026-02-26 11:19:33 +0000

    refactor Activites class by removing unused methods and associations for improved clarity

--- src/main/java/com/example/roko/entity/Activites.java
+++ src/main/java/com/example/roko/entity/Activites.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 import java.util.HashSet;
 import java.util.Set;
 
-
 @Entity
 @Table(name = "activites")
 @Data
@@ -26,31 +25,10 @@ public class Activites {
     @Column(length = 1000)
     private String description;
 
-
-    @OneToMany(mappedBy = "activite", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<Activites_Voyages> activitesVoyages = new HashSet<>();
-
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "voyage_id", nullable = false)
+    private Voyages voyage;
 
     @ManyToMany(mappedBy = "activites")
     private Set<Reservations> reservations = new HashSet<>();
-
-
-    public void addVoyage(Voyages voyage, Boolean obligatoire, Integer ordre) {
-        Activites_Voyages activiteVoyage = new Activites_Voyages();
-        activiteVoyage.setActivite(this);
-        activiteVoyage.setVoyage(voyage);
-        activiteVoyage.setObligatoire(obligatoire);
-        activiteVoyage.setOrdreAffichage(ordre);
-        activiteVoyage.setDisponible(true);
-
-        activitesVoyages.add(activiteVoyage);
-        voyage.getActivitesVoyages().add(activiteVoyage);
-    }
-
-    public void removeVoyage(Voyages voyage) {
-        activitesVoyages.removeIf(av ->
-                av.getActivite().equals(this) && av.getVoyage().equals(voyage));
-        voyage.getActivitesVoyages().removeIf(av ->
-                av.getActivite().equals(this) && av.getVoyage().equals(voyage));
-    }
 }
