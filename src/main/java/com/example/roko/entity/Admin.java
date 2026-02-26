9232091bbf29e003fd21be7cdd9afd715bc9d416
Hichamelhmaydi <ct1594065@gmail.com>
2026-02-26 11:35:48 +0000

    add Admin entity with discriminator value for user hierarchy

--- src/main/java/com/example/roko/entity/Admin.java
+++ src/main/java/com/example/roko/entity/Admin.java
@@ -1,4 +1,9 @@
 package com.example.roko.entity;
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("ADMINISTRATEUR")
 public class Admin extends User {
 }
