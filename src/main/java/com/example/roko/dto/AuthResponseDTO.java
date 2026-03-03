82dd2bad541c0bcad993691d1313391e15bf3ddc
Hichamelhmaydi <ct1594065@gmail.com>
2026-03-03 10:19:14 +0000

    add AuthResponseDTO and MessageResponseDTO classes: define data transfer objects for authentication and messaging responses

--- src/main/java/com/example/roko/dto/AuthResponseDTO.java
+++ src/main/java/com/example/roko/dto/AuthResponseDTO.java
@@ -1,4 +1,20 @@
+
 package com.example.roko.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponseDTO {
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String role;
+    private String message;
 }
