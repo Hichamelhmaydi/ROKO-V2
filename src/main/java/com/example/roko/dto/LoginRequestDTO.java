808a43c9f4f3cfa0749efc65302ded72a94c386b
Hichamelhmaydi <ct1594065@gmail.com>
2026-03-03 10:16:42 +0000

    add LoginRequestDTO and RegisterRequestDTO classes: define data transfer objects with validation for user login and registration

--- src/main/java/com/example/roko/dto/LoginRequestDTO.java
+++ src/main/java/com/example/roko/dto/LoginRequestDTO.java
@@ -1,4 +1,21 @@
+
 package com.example.roko.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequestDTO {
+
+    @NotBlank(message = "L'email est obligatoire")
+    @Email(message = "Format d'email invalide")
+    private String email;
+
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    private String password;
 }
--- src/main/java/com/example/roko/dto/RegisterRequestDTO.java
+++ src/main/java/com/example/roko/dto/RegisterRequestDTO.java
@@ -1,4 +1,35 @@
+
 package com.example.roko.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterRequestDTO {
+
+    @NotBlank(message = "Le nom est obligatoire")
+    private String nom;
+
+    @NotBlank(message = "Le prénom est obligatoire")
+    private String prenom;
+
+    @NotBlank(message = "L'email est obligatoire")
+    @Email(message = "Format d'email invalide")
+    private String email;
+
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    @Size(min = 6, message = "Le mot de passe doit contenir au moins 6 caractères")
+    private String password;
+
+    @NotBlank(message = "Le téléphone est obligatoire")
+    private String telephone;
+
+    private String idNational;
+    private String dateExpiration;
 }
